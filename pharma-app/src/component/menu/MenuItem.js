131a5b4cbdb67615cdf244dd123126abae67ca4c
atcherdsd <tchernak.csg@gmail.com>
2023-01-12 23:11:46 +0300

    feat: add style to active link

--- pharma-app/src/component/menu/MenuItem.js
+++ pharma-app/src/component/menu/MenuItem.js
@@ -2,12 +2,31 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { NavLink } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import { menuButtonColors } from '../helpers/menuButtonColors';
+
+const CustomizedListItemButton = styled(ListItemButton)`
+  border-radius: 1rem 0 0 1rem;
+  :hover {
+    color: ${menuButtonColors.actionsColor};
+    background-color: ${menuButtonColors.hoverBackgroundColor};
+    transition: 0.3s;
+  }
+  &.active {
+    color: ${menuButtonColors.actionsColor};
+    background: linear-gradient(
+      45deg,
+      ${menuButtonColors.activePrimaryBackground},
+      ${menuButtonColors.activeSecondaryBackground}
+    );
+  }
+`;
 
 export default function MenuItem({ text, route, icon }) {
   return (
-    <ListItemButton component={NavLink} to={route}>
+    <CustomizedListItemButton component={NavLink} to={route}>
       <ListItemIcon>{icon}</ListItemIcon>
       <ListItemText primary={text} />
-    </ListItemButton>
+    </CustomizedListItemButton>
   );
 }
