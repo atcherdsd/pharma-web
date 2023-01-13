b36b222698467f13dc243e8d234f18e4a4a912ec
atcherdsd <tchernak.csg@gmail.com>
2023-01-13 14:57:08 +0300

    Resolve merge conflict

--- pharma-app/src/component/AppBar.js
+++ pharma-app/src/component/AppBar.js
@@ -1,11 +1,17 @@
+import * as React from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 import MuiAppBar from '@mui/material/AppBar';
 import Badge from '@mui/material/Badge';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import IconButton from '@mui/material/IconButton';
 import { styled } from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import removeUserDataFromLS from './helpers/utils';
+import { useNavigate } from 'react-router-dom';
 
 const drawerWidth = 240;
 
@@ -28,6 +34,21 @@ const CustomAppBar = styled(MuiAppBar, {
 }));
 
 export default function AppBar({ open, toggleDrawer }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const navigate = useNavigate();
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleLogout = () => {
+    removeUserDataFromLS();
+    handleClose();
+    navigate('/');
+  };
+
   return (
     <CustomAppBar position="absolute" open={open}>
       <Toolbar
@@ -50,11 +71,45 @@ export default function AppBar({ open, toggleDrawer }) {
         <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
           Dashboard
         </Typography>
-        <IconButton color="inherit">
+        <IconButton
+          color="inherit"
+          sx={{
+            marginLeft: '12px',
+          }}
+        >
           <Badge badgeContent={4} color="secondary">
             <NotificationsIcon />
           </Badge>
         </IconButton>
+        <IconButton
+          sx={{
+            marginLeft: '12px',
+          }}
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="dialog"
+          onClick={handleMenu}
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
       </Toolbar>
     </CustomAppBar>
   );
