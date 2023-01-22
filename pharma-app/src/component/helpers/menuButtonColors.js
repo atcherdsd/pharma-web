9accba116cea1b0035fa9c693301ba75222c2b7e
atcherdsd <94963661+atcherdsd@users.noreply.github.com>
2023-01-22 17:01:16 +0300

    fix: remove extra file with colors

--- pharma-app/src/component/helpers/menuButtonColors.js
+++
@@ -1,6 +0,0 @@
-export const menuButtonColors = {
-  actionsColor: '#064406',
-  hoverBackgroundColor: 'rgba(0, 0, 0, 0.1)',
-  activePrimaryBackground: '#77bbff',
-  activeSecondaryBackground: '#fafafa',
-};
